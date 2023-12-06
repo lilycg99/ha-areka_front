3a648d2abdfcc6b453ae47b864b547d0b54faa0f
lilycg99 <lilycabreragrau@gmail.com>
2023-12-06 05:11:31 -0500

    Read me eliminado

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/HA-AREKA_FRONT/"
+  publicPath: '/HA-AREKA_FRONT/'
 })
